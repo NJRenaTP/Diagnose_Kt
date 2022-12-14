b7bde5bfced2d8dda7503621cf274ed091082f2c
Rena Ng Jing Ru <njrena@Renas-MacBook-Air.local>
2022-12-14 13:28:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asp/mainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
